f8e43425298b5ec0e43040c045cefd1a2a70cb3e
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-06-18 21:34:49 +0530

    Add files via upload

---
+++ Prog1.py
@@ -0,0 +1,12 @@
+number1 = [1, 2, 3]
+number2 = [4, 5, 6]
+result = map(lambda x, y: x + y, number1, number2)
+print("Addition of two lists : ")
+print(list(result))
+
+nums = [1, 2, 3, 4, 5]
+def sq(n):
+    return n*n
+square = list(map(sq, nums))
+print("Square of numbers in list : ")
+print(square)
